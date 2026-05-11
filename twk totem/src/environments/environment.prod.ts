30bbcf80d9646bce4a6968917e51f0378ed35992
sebastiano-speroni <sebastiano.speroni@jmconsulting.it>
2026-05-11 18:31:10 +0200

    Configurazioni environment

--- twk totem/src/environments/environment.prod.ts
+++ twk totem/src/environments/environment.prod.ts
@@ -1,6 +1,8 @@
 import packageInfo from '../../package.json';
 
 export const environment = {
+
+  //TWK DEV
   appVersion: packageInfo.version,
   production: true,
   clientUrl: "http://192.168.0.141",
@@ -18,6 +20,66 @@ export const environment = {
   company: "TWINPACK",
   key: "jmAdiutoNav2024",
   mock: false
+
+
+  //TWK PROD
+  // appVersion: packageInfo.version,
+  // production: true,
+  // clientUrl: "http://192.168.0.141",
+  // serverUrl: "http://192.168.0.141",
+  // urlFolder: "NAVWS/publish",
+  // serverUrlApi: "/api/",
+  // serverUrlApiOData: "/OData/Company('TWINPACK')/",
+  // serverUrlApiSoap: "/WS/TWINPACK/Codeunit/",
+  // clientUrlApp: "app/",
+  // clientPort: "",
+  // serverPortOData: ":7068/",
+  // serverPortSoap: ":7067/",
+  // redirecTo: "http://tpnav18app.twinpack.local/",
+  // navAutorization: "VE9URU06T3NpcmlkZTIwMjYh",
+  // company: "TWINPACK",
+  // key: "jmAdiutoNav2024",
+  // mock: false
+
+
+//TWN DEV
+  // appVersion: packageInfo.version,
+  // production: true,
+  // clientUrl: "http://192.168.0.141",
+  // serverUrl: "http://192.168.0.141",
+  // urlFolder: "NAVWS_DEV/publish",
+  // serverUrlApi: "/api/",
+  // serverUrlApiOData: "/OData/Company('TWINOVA')/",
+  // serverUrlApiSoap: "/WS/TWINOVA/Codeunit/",
+  // clientUrlApp: "app/",
+  // clientPort: "",
+  // serverPortOData: ":7068/",
+  // serverPortSoap: ":7067/",
+  // redirecTo: "http://tpnav18app.twinpack.local/",
+  // navAutorization: "VE9URU06T3NpcmlkZTIwMjYh",
+  // company: "TWINOVA",
+  // key: "jmAdiutoNav2024",
+  // mock: false
+
+
+  //TWN PROD
+  // appVersion: packageInfo.version,
+  // production: true,
+  // clientUrl: "http://192.168.0.141",
+  // serverUrl: "http://192.168.0.141",
+  // urlFolder: "NAVWS/publish",
+  // serverUrlApi: "/api/",
+  // serverUrlApiOData: "/OData/Company('TWINOVA')/",
+  // serverUrlApiSoap: "/WS/TWINOVA/Codeunit/",
+  // clientUrlApp: "app/",
+  // clientPort: "",
+  // serverPortOData: ":7068/",
+  // serverPortSoap: ":7067/",
+  // redirecTo: "http://tpnav18app.twinpack.local/",
+  // navAutorization: "VE9URU06T3NpcmlkZTIwMjYh",
+  // company: "TWINOVA",
+  // key: "jmAdiutoNav2024",
+  // mock: false
 
   
   // appVersion: packageInfo.version,
